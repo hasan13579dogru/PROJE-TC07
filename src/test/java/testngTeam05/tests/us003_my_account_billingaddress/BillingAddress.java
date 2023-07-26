a87a802618f202b666be9327ded73fe74e1c2007
mustafa <aslanbird940@gmail.com>
2023-07-26 21:16:39 +0300

    v2

--- src/test/java/testngTeam05/tests/us003_my_account_billingaddress/BillingAddress.java
+++ src/test/java/testngTeam05/tests/us003_my_account_billingaddress/BillingAddress.java
@@ -4,11 +4,16 @@ import org.testng.annotations.Test;
 import testngTeam05.utilities.ConfigReader;
 import testngTeam05.utilities.Driver;
 
+
 public class BillingAddress {
+
+
+
     @Test
     public void test01() {
 
-        Driver.getDriver().get(ConfigReader.getProperty("allovercommerceUrl"));
+       //Go to home page
+     Driver.getDriver().get(ConfigReader.getProperty("allovercommerceUrl"));
 
 
     }
