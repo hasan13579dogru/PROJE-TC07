a0e41eb67967fb165471b8e03d4f70e9fcf79f7b
livdemirci <livdemirci@gmail.com>
2023-07-27 21:08:09 +0300

    v1

--- src/test/java/testngTeam05/tests/us017_vendoralisveris/TC_01_VendorAlisVeris.java
+++ src/test/java/testngTeam05/tests/us017_vendoralisveris/TC_01_VendorAlisVeris.java
@@ -1,12 +1,21 @@
 package testngTeam05.tests.us017_vendoralisveris;
 
 import com.github.javafaker.Faker;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import testngTeam05.pages.AlloverCommercePage;
 import testngTeam05.utilities.ConfigReader;
 import testngTeam05.utilities.Driver;
+import testngTeam05.utilities.ReusableMethods;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.By.tagName;
+import static org.openqa.selenium.By.xpath;
+
 
 public class TC_01_VendorAlisVeris {
     //Vendor olarak alışveriş yapabilmeliyim.(My Account - Orders - Browse Product)
@@ -18,47 +27,100 @@ public class TC_01_VendorAlisVeris {
     //Place Order'a tıklanark alışverişin tamamlandığı görülebilmeli
     //My Account -Orders yapılan alışverişin ayrıntıları görülebilmeli
 
-    AlloverCommercePage  alloverpage;
+    AlloverCommercePage alloverpage;
     Faker faker;
+
     @Test
     public void test01() {
-        //Siteye git
-        Driver.getDriver().get(ConfigReader.getProperty("allovercommerceUrl"));
-        alloverpage = new AlloverCommercePage();
-
-        //Register butonuna tikla
-        alloverpage.register.click();
-        //Username, Your Email address, Password alanlarini doldur ve "I agree to the privacy policy" checkbox'ina tikla
-        //SIGN UP butonuna bas
-        alloverpage.reg_username.sendKeys(ConfigReader.getProperty("alloverMusteriName"),
-                Keys.TAB,
-                ConfigReader.getProperty("alloverVendorEmail"),
-                Keys.TAB,
-                ConfigReader.getProperty("alloverVendorSifre"),
-                Keys.TAB,Keys.TAB,Keys.SPACE,Keys.TAB,Keys.ENTER);
-
-        String alreadyRegistered = alloverpage.accountAlreadyRegisteredText.getText();
-
-        if (alreadyRegistered.equals("An account is already registered with your email address. Please log in.")){
-            vendorRegister();
-        }
-
+        vendorOlarakKayitOl();
+        //Vendor olorak kayit yapildigini dogrula
+        Assert.assertTrue(alloverpage.WelcometoAlloverCommerce.isDisplayed());
     }
 
-    public void vendorRegister (){
+
+    public String getEmailAdress() {
+
+        Driver.getDriver().switchTo().newWindow(WindowType.TAB);//yeni sekmede aciyorum
+        Driver.getDriver().get("https://www.fakemail.net/");
+
+        String fakeEmailAdress = Driver.getDriver().findElement(xpath("//*[@id='email']")).getText();
+        ReusableMethods.switchToWindow(0);
+        return fakeEmailAdress;
+    }
+
+    public String getVerificationCode() {
+        ReusableMethods.switchToWindow(1);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        By xpath = xpath("//*[contains(text(), '[Allover Commerce]')]");
+        wait.until(ExpectedConditions.invisibilityOfElementWithText(xpath, "Email Verification"));
+        Driver.getDriver().findElement(xpath("//*[@id=\"schranka\"]//tr[1]//td[1]")).click();
+        WebElement iframe = Driver.getDriver().findElement(xpath("//iframe[@id='iframeMail']"));
+        Driver.getDriver().switchTo().frame(iframe);
+
+        String code = Driver.getDriver().findElement(tagName("b")).getText();
+        System.out.println(code);
+        Driver.getDriver().switchTo().defaultContent();
+
+        ReusableMethods.switchToWindow(0);
+        return code;
+    }
+
+    public void vendorOlarakKayitOl(){
+
+        alloverpage = new AlloverCommercePage();
         faker = new Faker();
-        String vendorMusteriName = faker.name().toString();
-        String vendorEmailAdress = faker.internet().emailAddress();
-        String vendorPassword = faker.internet().password();
+        Driver.getDriver().get(ConfigReader.getProperty("allovercommerceUrl"));
+
+
+
         //Register butonuna tikla
         alloverpage.register.click();
-        //Username, Your Email address, Password alanlarini doldur ve "I agree to the privacy policy" checkbox'ina tikla
-        //SIGN UP butonuna bas
-        alloverpage.reg_username.sendKeys(vendorMusteriName,
-                Keys.TAB,
-                vendorEmailAdress,
-                Keys.TAB,
-                vendorPassword,
-                Keys.TAB,Keys.TAB,Keys.SPACE,Keys.TAB,Keys.ENTER);
+
+        //"Signup as a vendor?" yazisina tiklat.
+        alloverpage.register_as_vendor.click();
+        //"Vendor Registration" yazisinin gorundugunu dogrula
+        String vendorRegistration = "Vendor Registration";
+        String vendorRegistrationText = alloverpage.vendorRegistration.getText();
+        Assert.assertTrue(vendorRegistration.equals(vendorRegistrationText));
+
+
+        //Vendor sayfasindaki e-mail'e e-mail gir
+        String fakeEmail = getEmailAdress();
+
+        String rastgeleKelime = faker.lorem().word();
+        String rastgeleSayi = faker.number().digits(4);
+        String password = rastgeleKelime + rastgeleSayi;
+
+        alloverpage.vendorRegistrationEmail.sendKeys(fakeEmail, Keys.TAB);
+
+        //Send code butonuna tiklat
+        alloverpage.verificationEmailButton.click();
+
+
+        alloverpage.verificationCode.sendKeys(getVerificationCode(), Keys.TAB, Keys.TAB, password,
+                Keys.TAB, password, Keys.TAB, Keys.ENTER);
+    }
+
+    @Test
+    public void test02() {
+        //Vendor olarak alışveriş yapabilmeliyim.(My Account - Orders - Browse Product)
+        //Ürün ve ürünler seçilip sepete eklenebilmeli
+        //Chart - Chekout yapılarak alınacak ürün ve ürünler görülebilmeli
+        //Fatura ayrıntıları (BILLING DETAILS) doldurulabilmeli
+        //Toplam ödenecek rakam görüntülebilmeli
+        //Wire transfer/EFT veya Pay at the door seçenekleri seçilebilmeli
+        //Place Order'a tıklanark alışverişin tamamlandığı görülebilmeli
+        //My Account -Orders yapılan alışverişin ayrıntıları görülebilmeli
+        //Siteye git
+        vendorOlarakKayitOl();
+        //vendor olarak giris yapildi
+        alloverpage.WelcometoAlloverCommerce.click();
+
+        //Sayfa altindaki My Account'a tiklat
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+        ReusableMethods.click(alloverpage.myAccountButton);
+
+
     }
 }
