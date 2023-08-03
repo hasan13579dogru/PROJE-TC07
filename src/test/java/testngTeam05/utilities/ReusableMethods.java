c643000d4afc42cce4cb244c0606a4342b8148db
Mehtap Cakiroglu <mhtpckrgl@gmail.com>
2023-08-03 18:26:16 +0300

    03mehtap

--- src/test/java/testngTeam05/utilities/ReusableMethods.java
+++ src/test/java/testngTeam05/utilities/ReusableMethods.java
@@ -22,6 +22,263 @@ import java.util.List;
 import java.util.Random;
 
 public class ReusableMethods {
+    //HARD WAIT METHOD
+    public static void bekle(int saniye) {
+        try {
+            Thread.sleep(saniye * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Alert ACCEPT
+    public static void alertAccept() {
+        Driver.getDriver().switchTo().alert().accept();
+    }
+
+    //Alert DISMISS
+    public static void alertDismiss() {
+        Driver.getDriver().switchTo().alert().dismiss();
+    }
+
+    //Alert getText()
+    public static void alertText() {
+        Driver.getDriver().switchTo().alert().getText();
+    }
+
+    //Alert promptBox
+    public static void alertprompt(String text) {
+        Driver.getDriver().switchTo().alert().sendKeys(text);
+    }
+
+    //DropDown VisibleText
+    /*
+        Select select2 = new Select(gun);
+        select2.selectByVisibleText("7");
+
+        //ddmVisibleText(gun,"7"); --> Yukarıdaki kullanım yerine sadece method ile handle edebilirim
+     */
+    public static void ddmVisibleText(WebElement ddm, String secenek) {
+        Select select = new Select(ddm);
+        select.selectByVisibleText(secenek);
+    }
+
+    //DropDown Index
+    public static void ddmIndex(WebElement ddm, int index) {
+        Select select = new Select(ddm);
+        select.selectByIndex(index);
+    }
+
+    //DropDown Value
+    public static void ddmValue(WebElement ddm, String secenek) {
+        Select select = new Select(ddm);
+        select.selectByValue(secenek);
+    }
+
+    //SwitchToWindow1
+    public static void switchToWindow(int sayi) {
+        List<String> tumWindowHandles = new ArrayList<String>(Driver.getDriver().getWindowHandles());
+        Driver.getDriver().switchTo().window(tumWindowHandles.get(sayi));
+    }
+
+    //SwitchToWindow2
+    public static void window(int sayi) {
+        Driver.getDriver().switchTo().window(Driver.getDriver().getWindowHandles().toArray()[sayi].toString());
+    }
+    //EXPLICIT WAIT METHODS
+
+    //Visible Wait
+    public static void visibleWait(WebElement element, int sayi) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(sayi));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    //VisibleElementLocator Wait
+    public static WebElement visibleWait(By locator, int sayi) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(sayi));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    //Alert Wait
+    public static void alertWait(int sayi) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(sayi));
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+    //Tüm Sayfa ScreenShot
+    public static String tumSayfaResmi(String name) {
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+        String dosyaYolu = System.getProperty("user.dir") + "/target/Screenshots/" + name + tarih + ".png";
+        try {
+            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE), new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return dosyaYolu;
+    }
+
+    //WebElement ScreenShot
+    public static void webElementResmi(WebElement element) {
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "TestOutput/screenshot/webElementScreenshot" + tarih + ".png";
+
+        try {
+            FileUtils.copyFile(element.getScreenshotAs(OutputType.FILE), new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //WebTable
+    public static void printData(int satir, int sutun) {
+        WebElement satirSutun = Driver.getDriver().findElement(By.xpath("(//tbody)[1]//tr[" + satir + "]//td[" + sutun + "]"));
+        System.out.println(satirSutun.getText());
+    }
+
+    //Click Method
+    public static void click(WebElement element) {
+        try {
+            element.click();
+        } catch (Exception e) {
+            JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+            js.executeScript("arguments[0].click();", element);
+        }
+    }
+
+    //JS Scroll
+    public static void scroll(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    //JS Sayfa Sonu Scroll
+    public static void scrollEnd() {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+    }
+
+    //JS Sayfa Başı Scroll
+    public static void scrollHome() {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("window.scrollTo(0,-document.body.scrollHeight)");
+    }
+
+    //JS SendKeys
+    public static void sendKeysJS(WebElement element, String text) {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].value='" + text + "'", element);
+    }
+
+    //JS SendAttributeValue
+    public static void sendAttributeJS(WebElement element, String text) {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].setAttribute('value','" + text + "')", element);
+    }
+
+    //JS GetAttributeValue
+    public static void getValueByJS(String id, String attributeName) {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        String attribute_Value = (String) js.executeScript("return document.getElementById('" + id + "')." + attributeName);
+        System.out.println("Attribute Value: = " + attribute_Value);
+    }
+
+    // Rastgele bir kullanıcı adı oluşturmak için metot
+    public static String generateRandomUsername() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!.,€@#?$%^&abcdefghijklmnopqrstuvwxyz1234567890";
+        StringBuilder username = new StringBuilder();
+        Random rand = new Random();
+        int length = 12; // Kullanıcı adı uzunluğunu ayarlayın (örnekte 12 karakter)
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = rand.nextInt(characters.length());
+            char randomChar = characters.charAt(randomIndex);
+            username.append(randomChar);
+        }
+        return username.toString();
+    }
+
+    //Resim yüklemek için Robot methodu
+    public static void uploadFilePath(String filePath) {
+        try {
+            bekle(3);
+            StringSelection stringSelection = new StringSelection(filePath);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            bekle(3);
+            robot.keyPress(KeyEvent.VK_V);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_V);
+            bekle(3);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            bekle(3);
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //logout method
+    public static void logout() {
+        AlloverCommercePage alloversPage = new AlloverCommercePage();
+        ReusableMethods.click(alloversPage.signOut);
+        ReusableMethods.visibleWait(alloversPage.logout, 5);
+        alloversPage.logout.click();
+    }
+
+
+    //Kupon girisi sepet sayfası
+
+    public static void kuponGirisiSepetSayfasi(String kuponanahtari) {
+        AlloverCommercePage alloversPage = new AlloverCommercePage();
+
+
+        alloversPage.enterCoupon.sendKeys(ConfigReader.getProperty(kuponanahtari));
+        ReusableMethods.scroll(alloversPage.applyCoupon);
+        ReusableMethods.bekle(2);
+        alloversPage.applyCoupon.click();
+        ReusableMethods.bekle(3);
+        System.out.println(alloversPage.couponAlert.getText());
+    }
+
+
+    //clear to cart method
+    public static void clearCard() {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        AlloverCommercePage alloversPage = new AlloverCommercePage();
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+        ReusableMethods.click(alloversPage.cart);
+        ReusableMethods.bekle(2);
+        ReusableMethods.click(alloversPage.cartInClearProduct);
+        ReusableMethods.bekle(2);
+        alloversPage.close.click();
+    }
+
+    // Vendor olarak Sign In yapma methodu
+    public static void vendorSignIn() {
+        Driver.getDriver().get(ConfigReader.getProperty("allovercommerceUrl"));
+        AlloverCommercePage alloverPage = new AlloverCommercePage();
+        alloverPage.signIn.click();
+        alloverPage.signInUserNameOrEmail.sendKeys(ConfigReader.getProperty("alloverVendorEmail"), Keys.TAB,
+                ConfigReader.getProperty("alloverVendorSifre"));
+        alloverPage.signInButton.click();
+    }
+
+    //Kupon girisi odeme sayfası
+    public static void kuponGirisiodemeSayfasi(String kuponanahtari) {
+        AlloverCommercePage alloversPage = new AlloverCommercePage();
+        alloversPage.enterTCouponBillAdress.click();
+        alloversPage.enterCouponBoxBillAdress.sendKeys(ConfigReader.getProperty(kuponanahtari));
+        alloversPage.applyCouponBillAdress.click();
+        ReusableMethods.bekle(2);
+        System.out.println(alloversPage.couponAlert.getText());
+
+    }
+
 
 
 }
